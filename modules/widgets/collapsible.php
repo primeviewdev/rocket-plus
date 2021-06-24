32ad65cd216c90835c8819608010df643c740ff1
JanfredMagnaye <ryukusho11@gmail.com>
2021-06-24 12:48:17 -0700

    Fix Undefined Index errors with new Defaults

--- modules/widgets/collapsible.php
+++ modules/widgets/collapsible.php
@@ -2,7 +2,7 @@
 
 // Creating the widget 
 class collapsible_widget extends WP_Widget {
- 
+    protected $defaults;
     // The construct part  
     function __construct() {
         parent::__construct(
@@ -16,25 +16,46 @@ class collapsible_widget extends WP_Widget {
             // Widget description
             array( 'description' => __( 'Add Bootstrap Collapsible to your sidebars', 'collapsible_widget_domain' ), ) 
         );
+        // Setup default values
+        $this->defaults = array(
+            'title'  => 'Collapsible Header',
+            'title_tag' => 'span',
+            'title_type'  => 'button',
+            'identifier'  => 'collapsible-identifier',
+            'content'  => 'Collapsible Content',
+        );
     }
       
     // Creating widget front-end
     public function widget( $args, $instance ) {
+        // Call for Defaults
+        $instance = wp_parse_args( (array) $instance, $this->defaults );
+
         $title = apply_filters( 'widget_title', $instance['title'] );
         $title_tag =  apply_filters( 'widget_title', $instance['title_tag'] );
+        $title_type = $instance['title_type'];
         $identifier = apply_filters( 'widget_title', $instance['identifier'] );
         $widget_id = $args['id'];
         $content = $instance['content'];
   
         // before and after widget arguments are defined by themes
         echo '<div class="collapsible-header">';
-            echo '<button type="button" data-toggle="collapse" data-target="#'.$identifier.'" aria-expanded="false" aria-controls="'.$identifier.'">';
-            if ( ! empty( $title ) )
-                echo '<'.$title_tag.' class="card-title">' . $title . '</'.$title_tag.'>';
-            echo '</button>';
+            if($title_type=='link') {
+                echo '  <a class="collapsible-header-link" data-toggle="collapse" href="#'.$identifier.'" role="button" aria-expanded="false" aria-controls="'.$identifier.'">';
+            } else {
+                echo '<button class="collapsible-header-button" type="button" data-toggle="collapse" data-target="#'.$identifier.'" aria-expanded="false" aria-controls="'.$identifier.'">';
+            }
+            if ( ! empty( $title ) ) {
+                echo '<'.$title_tag.' class="collapsible-title">' . $title . '</'.$title_tag.'>';
+            }
+            if($title_type=='link') {
+                echo '</a>';
+            } else {
+                echo '</button>';
+            }
         echo '</div>';
         echo '<div id="'.$identifier.'" class="collapse">';
-            echo '<div class="card-content">';  
+            echo '<div class="collapsible-content">';  
             echo apply_filters('the_content',$content); 
             echo '</div>';            
         echo '</div>';
@@ -43,11 +64,15 @@ class collapsible_widget extends WP_Widget {
               
     // Creating widget Backend 
     public function form( $instance ) {
+        $instance = wp_parse_args( (array) $instance, $this->defaults );
         if ( !empty( $instance[ 'title' ] ) ) {
             $title = $instance[ 'title' ];
         }
         if ( !empty( $instance[ 'title_tag' ] ) ) {
             $title_tag = $instance[ 'title_tag' ];
+        }
+        if ( !empty( $instance[ 'title_type' ] ) ) {
+            $title_type = $instance[ 'title_type' ];
         }
         if ( !empty( $instance[ 'identifier' ] ) ) {
             $identifier = $instance[ 'identifier' ];
@@ -55,12 +80,14 @@ class collapsible_widget extends WP_Widget {
         if ( !empty( $instance[ 'content' ] ) ) {
             $content = $instance[ 'content' ];
         }
-        else {
-            $title = __( 'Collapsible Header', 'collapsible_widget_domain' );
-            $title_tag = __( 'span', 'collapsible_widget_domain' );
-            $identifier = __( 'collapsible-identifier', 'collapsible_widget_domain' );
-            $content = __( 'Collapsible Content', 'collapsible_widget_domain' );
-        }
+        // Default Fallback values
+        // else {
+        //     $title = __( 'Collapsible Header', 'collapsible_widget_domain' );
+        //     $title_tag = __( 'span', 'collapsible_widget_domain' );
+        //     $title_type = __( 'button', 'collapsible_widget_domain' );
+        //     $identifier = __( 'collapsible-identifier', 'collapsible_widget_domain' );
+        //     $content = __( 'Collapsible Content', 'collapsible_widget_domain' );
+        // }
         // Widget admin form
         ?>
             <p>
@@ -78,7 +105,19 @@ class collapsible_widget extends WP_Widget {
                 />
             </p>
             <p>
-                <label for="<?php echo $this->get_field_id( 'identifier' ); ?>"><?php _e( 'Identifier:' ); ?></label> 
+                <label for="<?php echo $this->get_field_id( 'title_type' ); ?>"><?php _e( 'Title Type:' ); ?></label> 
+                <select class='widefat' id="<?php echo $this->get_field_id('title_type'); ?>"
+                        name="<?php echo $this->get_field_name('title_type'); ?>" type="text">
+                <option value='button'<?php echo ($title_type=='button')?'selected':''; ?>>
+                    Button &lt;button&gt;
+                </option>
+                <option value='link'<?php echo ($title_type=='link')?'selected':''; ?>>
+                    Link &lt;a&gt;
+                </option> 
+                </select>     
+            </p>
+            <p>
+                <label for="<?php echo $this->get_field_id( 'identifier' ); ?>"><?php _e( 'Identifier ( This should be a unique value to all collapsible widgets used ):' ); ?></label> 
                 <input class="widefat" id="<?php echo $this->get_field_id( 'identifier' ); ?>" name="<?php echo $this->get_field_name( 'identifier' ); ?>" type="text" value="<?php echo esc_attr( $identifier ); ?>" required />
             </p>
             <p>
@@ -95,6 +134,7 @@ class collapsible_widget extends WP_Widget {
         $instance = array();
         $instance['title'] = ( ! empty( $new_instance['title'] ) ) ? strip_tags( $new_instance['title'] ) : '';
         $instance['title_tag'] = ( ! empty( $new_instance['title_tag'] ) ) ? strip_tags( $new_instance['title_tag'] ) : '';
+        $instance['title_type'] = $new_instance['title_type'];
         $instance['identifier'] = ( ! empty( $new_instance['identifier'] ) ) ? strip_tags( $new_instance['identifier'] ) : '';
         $instance['content'] = $new_instance['content'];
         return $instance;
